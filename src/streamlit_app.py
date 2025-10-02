a028a6a8243a31dc5fbff9473402d0bfd1246e21
CarlaHL <155692380+CarlaHL@users.noreply.github.com>
2025-10-01 21:52:56 -0500

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -16,10 +16,29 @@ Our code is hosted in the following GithHub repository: https://github.com/
      )
 
 with tab2:
-    st.markdown("### Static Maps & Department Analysis")
+     st.markdown("### Static Maps & Department Analysis")
     
-    st.image("hospital_dept_bars.png", caption="Distribution of hospitals across departments", use_container_width=True)
-    st.image("hospital_dept_map.png", caption="Geospatial distribution of hospitals by department (choropleth map)", use_container_width=True)
+     import streamlit as st
+     import matplotlib.pyplot as plt
+     import seaborn as sns
+
+     #Título
+     st.title("Hospitals by Department")
+
+     # Aquí asumo que summary_table ya está definido
+     fig, ax = plt.subplots(figsize=(12, 6))
+     sns.barplot(
+     data=summary_table,
+     x="Departamento",
+     y="n_hospitals_dept",
+     ax=ax,
+     color="steelblue"
+     )
+     plt.xticks(rotation=90)
+     ax.set_title("Distribution of hospitals across departments")
+
+# Mostrar en Streamlit
+st.pyplot(fig)
 
 
 with tab3:
