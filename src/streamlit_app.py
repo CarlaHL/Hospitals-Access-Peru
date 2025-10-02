8a7e6d023f34d2655395f2b94e72582cc7069555
CarlaHL <155692380+CarlaHL@users.noreply.github.com>
2025-10-01 21:43:09 -0500

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -6,14 +6,14 @@ tab1, tab2, tab3 = st.tabs(["Data Desctription", "Static Maps & Department Analy
 
 with tab1:
 
-        st.markdown(
+     st.markdown(
 """
 # Data Desctription
 The purpose of this application is to present a geospatial analysis of hospitals in Peru, both at the departmental level and at the population center level. Through this visualization, we aim to identify patterns in their geographic distribution as well as potential gaps in access to healthcare services.
 
 Our code is hosted in the following GithHub repository: https://github.com/
 """
-        )
+     )
 
 with tab2:
     st.markdown("### Static Maps & Department Analysis")
@@ -21,11 +21,6 @@ with tab2:
     st.image("hospital_dept_bars.png", caption="Distribution of hospitals across departments", use_container_width=True)
     st.image("hospital_dept_map.png", caption="Geospatial distribution of hospitals by department (choropleth map)", use_container_width=True)
 
+
 with tab3:
-        st.markdown("""
-### Dynamic Maps
-"""
-)
-
-
-
+    st.markdown"Dynamic Maps")
